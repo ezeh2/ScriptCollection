c591836dc64bf032cafb68ba5c620b8b69b45bf4
Edward <e.zeh@gmx.ch>
2021-07-03 19:57:25 +0200

    find_all_host weiterentwickelt

--- bash/find_all_hosts_with_reverse_dns_lookup.sh
+++ bash/find_all_hosts_with_reverse_dns_lookup.sh
@@ -4,16 +4,25 @@
 # this allows to get a list of recently used IP adresses,
 # which is much faster than ping
 
+adress_prefix=192.168.1
+adress_prefix_reverse=1.168.192
 
 for i in {0..255};do
-#	echo $i
+# echo $i
 # use PTR instead of -x, because -x does not support +noall +answer
-dns=`dig PTR +noall +answer $i.99.168.192.in-addr.arpa`
+dns=`dig PTR +noall +answer $i.$adress_prefix_reverse.in-addr.arpa`
+# number of characters in $dns>0 means dig found hostname for ip-adress
 c=`echo $dns | wc -m`
 if [ $c -gt 1 ]
 then
-	echo $dns
-	#ip=192.168.1.$i
-	#iping -c 1 $ip 
+	echo "dig: "$dns
+	# ping host so that mac-adress is in arp-table
+	nmap=`nmap -sn $adress_prefix.$i`
+	echo "nmap -sn: "$nmap
 fi
 done
+
+# this shows hostname, ip-adress and mac-adress
+arp -a
+
+
