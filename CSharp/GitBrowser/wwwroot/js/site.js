0c255b3d44f574d596b3e972645ad4cb1ea4395c
Edward Reutling <e.zeh@gmx.ch>
2025-06-09 17:07:31 +0200

    move js to site.js

--- CSharp/GitBrowser/wwwroot/js/site.js
+++ CSharp/GitBrowser/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).on('click', '.repo-link', function(e) {
+    e.preventDefault();
+    var path = $(this).data('path');
+    $('#commits').empty();
+    $.get('/Git/Branches', { repoPath: path }, function(data) {
+        $('#branches').html(data);
+    });
+});
+
+$(document).on('click', '.branch-link', function(e) {
+    e.preventDefault();
+    var path = $(this).data('path');
+    var branch = $(this).data('branch');
+    $.get('/Git/Log', { repoPath: path, branchName: branch }, function(data) {
+        $('#commits').html(data);
+    });
+});
+
+
+
+
