ffcdd9929dcd1a19695927a93125678cbc86e5ea
Edward Reutling <e.zeh@gmx.ch>
2025-06-09 10:32:48 +0200

    changed to partial view

--- CSharp/GitBrowser/Controllers/GitController.cs
+++ CSharp/GitBrowser/Controllers/GitController.cs
@@ -19,31 +19,31 @@ public class GitController : Controller
         return View(repos);
     }
 
-    public IActionResult Branches(string repoPath)
-    {
-        using var repo = new Repository(repoPath);
-        var branches = repo.Branches
-            .Where(b => !b.IsRemote)
-            .Select(b => new GitBranch { Name = b.FriendlyName })
-            .ToList();
+	public IActionResult Branches(string repoPath)
+	{
+		using var repo = new Repository(repoPath);
+		var branches = repo.Branches
+			.Where(b => !b.IsRemote)
+			.Select(b => new GitBranch { Name = b.FriendlyName })
+			.ToList();
 
-        ViewBag.RepoPath = repoPath;
-        return View(branches);
-    }
+		ViewBag.RepoPath = repoPath;
+		return PartialView("_BranchesPartial", branches);
+	}
 
-    public IActionResult Log(string repoPath, string branchName)
-    {
-        using var repo = new Repository(repoPath);
-        var branch = repo.Branches[branchName];
-        var commits = branch.Commits
-            .Select(c => new GitCommit
-            {
-                Sha = c.Sha,
-                Message = c.MessageShort,
-                Author = c.Author.Name,
-                Date = c.Author.When.DateTime
-            }).ToList();
+	public IActionResult Log(string repoPath, string branchName)
+	{
+		using var repo = new Repository(repoPath);
+		var branch = repo.Branches[branchName];
+		var commits = branch.Commits
+			.Select(c => new GitCommit
+			{
+				Sha = c.Sha,
+				Message = c.MessageShort,
+				Author = c.Author.Name,
+				Date = c.Author.When.DateTime
+			}).ToList();
 
-        return View(commits);
-    }
+		return PartialView("_LogPartial", commits);
+	}
 }
